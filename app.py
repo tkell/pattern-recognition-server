2a9c07c8ce2b39eaa02b9a77d4b8b85b359ed4db
tkell <thor.kell@mail.mcgill.ca>
2014-04-24 11:14:45 -0400

    Adding gitignore, classify

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Webserver.  This needs to take data from the mobile app, 
+classify it, and return a list of classifications.
+It will be the mobile app's responsibility to do 
+mapping based on the classifications (piano, grid, offset grid, ec)
+'''
+
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 
@@ -12,12 +23,16 @@ def classification_from_data():
     pass
 
 
-@app.route("/analysis")
+## We will eventually remove this GET thang.
+@app.route("/analysis", methods=['GET', 'POST'])
 def analyze_data():
-    classification_from_data()
+    print request.json
+
+
+    res = classification_from_data()
     return "We have, in theory, parsed the data and returned JSON"
 
-@app.route("/image")
+@app.route("/image", methods=['POST'])
 def analyze_image():
     return "We have, in theory, parsed the image and returned JSON"
 
---
+++ classify.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Classification tools.  Functions from here will be invoked by the webserver
+in order to classify data.  
+'''
