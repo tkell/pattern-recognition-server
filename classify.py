b2bcf0859728941d33b3ef40876b540026195a50
tkell <thor.kell@mail.mcgill.ca>
2014-04-24 11:28:00 -0400

    Reading files for classification

--- classify.py
+++ classify.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
+
 '''
 Classification tools.  Functions from here will be invoked by the webserver
-in order to classify data.  
+in order to classify data.
 '''
+
+def load_data_from_file(classification):
+    data = []
+    file_path = 'example_data/%s.json' % classification
+    with open(file_path, 'r') as f:
+        for line in f:
+            data.append(json.loads(line))
+    return data
