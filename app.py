6ec4e717578b721195485adbbef693f77eb78283
tkell <thor.kell@mail.mcgill.ca>
2014-05-03 19:42:37 -0400

    Trying my own ugly headers, without crossdomain

--- app.py
+++ app.py
@@ -59,7 +59,6 @@ def classification_from_data(example_data):
 
 ## We will eventually remove this GET thang.
 @app.route("/analysis", methods=['GET', 'POST', 'OPTIONS'])
-@crossdomain(origin='*', headers=['Content-Type'])
 def analyze_data():
     print request.json
 
